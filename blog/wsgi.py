3786fbb5a432fa72e066d0fafcef0dac5346471c
keitumetsi <76055087+sianda19@users.noreply.github.com>
2023-02-14 16:20:46 +0200

    Update wsgi.py

--- blog/wsgi.py
+++ blog/wsgi.py
@@ -1,9 +1,9 @@
 
-import os
+#import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
 
 application = get_wsgi_application()
 
